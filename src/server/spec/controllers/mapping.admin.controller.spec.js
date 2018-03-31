685fcf89fd58b9bd3375f72a4aa07bc838d01d0f
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-03-31 23:57:14 +0530

    Test cases added for add mapping

--- src/server/spec/controllers/mapping.admin.controller.spec.js
+++ src/server/spec/controllers/mapping.admin.controller.spec.js
@@ -6,18 +6,34 @@ describe('Server', () => {
         server = require('../../app');
     });
 
+    describe('While adding a new mapping', () => {
+        var data = {};
+        beforeAll((done) => {
+            request.post('http://localhost:3000/mapping/add', {json: true, body: { "key": "hp1", "value": "https://www.hp.com", "expiry": "2100-12-31" }}, 
+                    (error, response, body) => {
+                        data.status = response.statusCode;
+                        data.body = body;
+                        done();
+                    });
+        });
+        it ('should add a mapping if request is valid', () => {
+            expect(data.status).toEqual(201);
+        });
+    });
+
     describe('While deleting a new mapping,', () => {
         var data = {};
         beforeAll((done) => {
             request.delete('http://localhost:3000/mapping/hp1', (error, request, body) => {
                 data.status = request.statusCode;
-                data.response = body;
+                data.body = body;
                 done();
             });
         });
 
-        it ('should throw error if mapping does not exists by returning 500 error', () => {
-            expect(data.status).toBe(500);
+        it ('should remove the mapping removing status code 200', () => {
+            expect(data.status).toBe(200);
+            expect(data.body).toEqual('Removed mapping successfully!!');
         });
     });
 });
