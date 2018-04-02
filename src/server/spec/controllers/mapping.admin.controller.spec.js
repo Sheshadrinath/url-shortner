4368784e3e8ed21d588a01598efe2464ae9c0a85
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-04-02 21:49:46 +0530

    Test cases updated

--- src/server/spec/controllers/mapping.admin.controller.spec.js
+++ src/server/spec/controllers/mapping.admin.controller.spec.js
@@ -19,21 +19,53 @@ describe('Server', () => {
         it ('should add a mapping if request is valid', () => {
             expect(data.status).toEqual(201);
         });
-    });
 
-    describe('While deleting a new mapping,', () => {
-        var data = {};
-        beforeAll((done) => {
-            request.delete('http://localhost:3000/mapping/hp1', (error, request, body) => {
-                data.status = request.statusCode;
-                data.body = body;
-                done();
+        describe('if the mapping already exists by key', () => {
+            var data = {};
+            beforeAll((done) => {
+                request.post('http://localhost:3000/mapping/add', {json: true, body: { "key": "hp1", "value": "https://www.hp1.com", "expiry": "2100-12-31" }}, 
+                        (error, response, body) => {
+                            data.status = response.statusCode;
+                            data.body = body;
+                            done();
+                        });
+            });
+            it ('should return the existing object', () => {
+                expect(data.status).toEqual(201);
+                console.log(data.body);
             });
         });
 
-        it ('should remove the mapping removing status code 200', () => {
-            expect(data.status).toBe(200);
-            expect(data.body).toEqual('Removed mapping successfully!!');
+        describe('if the mapping already exists by value', () => {
+            var data = {};
+            beforeAll((done) => {
+                request.post('http://localhost:3000/mapping/add', {json: true, body: { "key": "hp", "value": "https://www.hewlettpackard.com", "expiry": "2100-12-31" }}, 
+                        (error, response, body) => {
+                            data.status = response.statusCode;
+                            data.body = body;
+                            done();
+                        });
+            });
+            it ('should return the existing object', () => {
+                expect(data.status).toEqual(201);
+                console.log(data.body);
+            });
+        });
+
+        describe('After successful addition, while deleting a new mapping,', () => {
+            var data = {};
+            beforeAll((done) => {
+                request.delete('http://localhost:3000/mapping/hp1', (error, request, body) => {
+                    data.status = request.statusCode;
+                    data.body = body;
+                    done();
+                });
+            });
+    
+            it ('should remove the mapping removing status code 200', () => {
+                expect(data.status).toBe(200);
+                expect(data.body).toEqual('Removed mapping successfully!!');
+            });
         });
     });
 });
