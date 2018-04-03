6ed7aec295adb04201d09fa2433a466eff2940d7
Sheshadrinath R <sheshadrinathr@Sheshadrinaths-MacBook-Pro.local>
2018-04-04 00:02:27 +0530

    Test case updated for random text generation

--- src/server/spec/controllers/mapping.random.controller.spec.js
+++ src/server/spec/controllers/mapping.random.controller.spec.js
@@ -6,10 +6,35 @@ describe('Server ', () => {
         server = require('../../app');
     });
 
-    describe('When requested for random key, then', () => {
+    describe('When requested for random key, and if the existing url is sent, then', () => {
         var data = {};
         beforeAll((done) => {
-            request.get("http://localhost:3000/random/generate", (error, request, body) => {
+            request.post("http://localhost:3000/random/generate", {json: true, body: { "value": "https://www.apple.com" }}, 
+            (error, request, body) => {
+                data.status = request.statusCode;
+                data.body = body[0];
+                done();     //This along with above done in beforeAll is required, since these calls are async in nature.
+            });
+        });
+        it('response code should be 200', () => {
+            expect(data.status).toBe(200);
+        });
+        it('response body should contain existing mapping', () => {
+            expect(data.body.key).not.toBeNull();
+            expect(data.body.key).not.toBeUndefined();
+            expect(data.body.value).toBe('https://www.apple.com');
+            expect(data.body.expiryDate).not.toBeNull();
+            expect(data.body.expiryDate).not.toBeUndefined();
+        });
+    });
+
+    describe('When requested for random key, and if the url is not present, then', () => {
+        var data = {};
+        var randomUrl = "https://www." + new Date().getTime().toString() + ".com";
+        console.log(randomUrl);
+        beforeAll((done) => {
+            request.post("http://localhost:3000/random/generate", {json: true, body: { "value": randomUrl }}, 
+            (error, request, body) => {
                 data.status = request.statusCode;
                 data.body = body;
                 done();     //This along with above done in beforeAll is required, since these calls are async in nature.
@@ -18,8 +43,12 @@ describe('Server ', () => {
         it('response code should be 200', () => {
             expect(data.status).toBe(200);
         });
-        it('response body should contain random text with 12 characters', () => {
-            expect(data.body.length).toBe(12);
+        it('response body should return new mapping', () => {
+            expect(data.body.key).not.toBeNull();
+            expect(data.body.key).not.toBeUndefined();
+            expect(data.body.value).toBe(randomUrl);
+            expect(data.body.expiry).not.toBeNull();
+            expect(data.body.expiry).not.toBeUndefined();
         });
     });
 });
